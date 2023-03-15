17c348da23a8dabe1f1f007638e2153db3a091c6
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-15 09:31:33 -0300

    feat: Configurando o Eslint e o editorconfig

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (request, response) => {
   response.send('Hello world');
-})
+});
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000')); 
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
