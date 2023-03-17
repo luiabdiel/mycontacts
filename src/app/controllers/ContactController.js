86cce8605df8f91cefb6fd5fbb0df6f1326bac19
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-17 15:07:05 -0300

    Criando a rota de edição de Contatos

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -32,7 +32,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already been take' });
+      return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -42,8 +42,31 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
-    // Editar UM registro
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactEmailExists = await ContactsRepository.findByEmail(email);
+    if (contactEmailExists && contactEmailExists.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
