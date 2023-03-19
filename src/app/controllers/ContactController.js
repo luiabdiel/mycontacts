2ef5aa8273da3e5322f0f7f7d5c28c5cdbab85b5
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-19 09:18:50 -0300

    refactor: Refatorando o método delete

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -73,13 +73,6 @@ class ContactController {
   async delete(request, response) {
     const { id } = request.params;
 
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404: Not Found
-      return response.status(404).json({ error: 'Contact not found' });
-    }
-
     await ContactsRepository.delete(id);
     // 204: No Content
     response.sendStatus(204);
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Ferreira',
-    email: 'ferreira@gmail.com',
-    phone: '99999999999',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Pedro',
-    email: 'pedro@gmail.com',
-    phone: '99999999991',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -60,11 +41,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
